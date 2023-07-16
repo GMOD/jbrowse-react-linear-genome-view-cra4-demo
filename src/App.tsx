3e14d349785ef823d05d9f2f269c414052fe2667
Colin <colin.diesh@gmail.com>
2023-07-16 12:53:01 -0400

    Bump deps

--- src/App.tsx
+++ src/App.tsx
@@ -42,11 +42,11 @@ function View() {
       <p>
         The code for this app is available at{' '}
         <a
-          href="https://github.com/GMOD/jbrowse-components/tree/main/embedded_demos/jbrowse-react-linear-genome-view"
+          href="https://github.com/GMOD/jbrowse-react-linear-genome-view-cra4-demo"
           target="_blank"
           rel="noreferrer"
         >
-          https://github.com/GMOD/jbrowse-components/tree/main/embedded_demos/jbrowse-react-linear-genome-view
+          https://github.com/GMOD/jbrowse-react-linear-genome-view-cra4-demo
         </a>
         .
       </p>
